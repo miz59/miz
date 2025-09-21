7f1e47f5d0f72b6744edf39327b60abeb7efb7cb
hosseinka6566 <hosseinka6566@gmail.com>
2025-09-21 20:02:40 +0330

    update scripts.js in theme

--- themes/scripts.js
+++ themes/scripts.js
@@ -1,21 +1,4 @@
-import fs from "fs";
-
-const data = await fs.promises.readFile("./package.json", "utf-8");
-const packageJson = JSON.parse(data);
-
-const buildMizCommand = packageJson.scripts["build-miz"];
-
-const regex = /(?:^|[\s:"'])((?:[\w\-./]+)?)(?=\/assets\/)/g;
-
-const matches = [];
-let match;
-while ((match = regex.exec(buildMizCommand)) !== null) {
-    matches.push(match[1]);
-}
-
-const uniquePaths = [...new Set(matches)];
-
 export const config = {
     theme: 'mizoon',
-    output: `${uniquePaths}/assets/js/mizchin.min.js` 
+    output: `/assets/js/mizchin.min.js`
 };
